fba0dc10c3f7bf2cf4761aaa24892771a930e332
Andrey Gorshkov <andrew@visec.com>
2018-10-30 20:36:39 +0200

    pretty output

--- gqltst/schema.py
+++ gqltst/schema.py
@@ -241,7 +241,10 @@ def node_from_data(data, types_cache, path=[]):
 class Schema(object):
     def __init__(self, url):
         self.url = url
+
+        print("Requesting structure...", end='\r', flush=True)
         structure = requests.get(self.url, params={"query": structure_query})
+        print("Building caches...", end='\r', flush=True)
 
         types_cache = {}
         self.schema_objects = {}
@@ -261,6 +264,8 @@ class Schema(object):
         for type_name, class_obj in SCALAR_TYPES.items():
             self.register_scalar(type_name, class_obj)
 
+        print("Initialization done!", end='\r', flush=True)
+
     def register_scalar(self, name, resolver):
         self.scalars[name] = resolver()
 
@@ -273,21 +278,33 @@ class Schema(object):
     def test(self, args={}, validators={}):
         self.prepare_test_queries()
         for test in self.test_queries:
-            print("Test %s" % (".".join(test.query_data.path)))
+            test_query_path = ".".join(test.query_data.path)
+            print("Test %s" % (test_query_path), end='\r', flush=True)
             test_iteration = 1
+            failed_tests = 0
             for query, values in test.get_query(self.scalars, args=args, validators=validators):
                 result = requests.get(self.url, params={"query": query})
                 if result.status_code == 200:
-                    print(" test #%s" % test_iteration, values)
+                    if failed_tests > 0:
+                        print("Test %s (%s/%s) - FAIL" % (test_query_path, failed_tests, test_iteration), end='\r', flush=True)
+                    else:
+                        print("Test %s (%s)" % (test_query_path, test_iteration), end='\r', flush=True)
                     response_data = result.json()
                     errors = self.validate_response(response_data["data"], validators)
+
                     if len(errors) > 0:
-                        print(query)
-                    print([str(r) for r in errors])
+                        failed_tests += 1
+                    #     print(query)
+                    # print([str(r) for r in errors])
                 else:
                     print(query, values)
 
                 test_iteration += 1
+
+            if failed_tests > 0:
+                print("Test %s (%s/%s) - FAIL" % (test_query_path, failed_tests, test_iteration))
+            else:
+                print("Test %s (%s/%s) - DONE" % (test_query_path, test_iteration, test_iteration))
 
     def validate_response(self, response_data, validators):
         for key in response_data.keys():
