86d9aff6f8a34c582fffa90b70516408017cd63d
Andrey Gorshkov <andrew@visec.com>
2018-10-30 17:37:14 +0200

    functional resolvers

--- gqltst/reslovers.py
+++ gqltst/reslovers.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime, timedelta
 
 def connection_first_resolver(context):
     for i in [None, random.randint(1, 5)]:
@@ -10,3 +11,26 @@ def connection_last_resolver(context):
             yield random.randint(1, 5)
 
     yield None
+
+def range_resolver(range=[]):
+    def func(context):
+        for i in range:
+            yield i
+    return func
+
+def depend_resolver(key, value, match_range=[], un_match_range=[]):
+    def func(context):
+        if context["vars"][key] == value:
+            for i in match_range:
+                yield i
+        else:
+            for i in un_match_range:
+                yield i
+    return func
+
+def days_ago_resolver(days):
+    def func(context):
+        now_date = datetime.now()
+        for r in [(now_date - timedelta(days=days)).strftime("%Y-%m-%d %H:%M:%S")]:
+            yield r
+    return func
